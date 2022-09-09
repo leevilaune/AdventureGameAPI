684e21ca18f4d3190504a12f3293ea9ff10d5b47
nuuskapoeka <npoeka@gmail.com>
2022-09-09 12:41:37 +0300

    removed appsettings from tacking and added Quest.cs

---
+++ AdventureGameAPI/Quest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdventureGameAPI
+{
+    public class Quest
+    {
+        [Key]
+        public int Id { get; set; }
+        public string QuestName { get; set; }
+        public string QuestDesc { get; set; }
+        public string QuestReward { get; set; }
+        public int QuestExp { get; set; }
+        public bool CompletionStatus { get; set; }
+
+    }
+}
